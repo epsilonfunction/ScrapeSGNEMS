f9843b6b0b82130a03708043cda95a973f574eae
Foo Jia Yuan <00492@pacificlight.com.sg>
2024-08-20 11:55:35 +0800

    feat(TPCScrape.py): Implemented Scraping function

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ScrapeSGNEMS
 Scraping NEMS Prices
+
+
+This repo is created to scrape SG NEMS data for reporting date. Sometimes, UIPath fails so we go Old School.
+
+Steps:
+1) $pip install -r requirements.txt
+2) Alter Parameters.json to desired date. Set the following:
+    1) "fromDate"
+    2) "toDate"
+    3) "filepath"
+3) $python Scraper.py
+
+Dependencies:
+1) Python 3.11. Probably works for earlier version. Not tested for anything outside of 3.11
+
+Future plans:
+1) Rewrite in golang for ease of use
+2) Containerisation
+3) Implement test cases (PRIORITY !!!)
---
+++ TPCScrape.py
@@ -0,0 +1,56 @@
+import os
+import time
+from urllib.parse import urlencode
+import requests
+import json
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Not Needed for now; generally a good idea to keep around for error handling
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+nems_url = "https://www.nems.emcsg.com/nems-prices"
+
+# Default Options; running in headless mode
+driveroptions = Options()
+driveroptions.add_argument("--headless")
+
+driver = webdriver.Edge(options=driveroptions)
+driver.get(nems_url) 
+selenium_cookies = driver.get_cookies()
+cookies = {cookie['name']: cookie['value'] for cookie in selenium_cookies}
+
+# driver.wait(10) TODO: Determine if wait time or WebDriverWait is better
+#TODO: Consider removing and hardcoding the form action url since it is porbably fixed.
+form_elem = driver.find_element(By.XPATH, "//form[@action='/api/sitecore/DataSync/DataDownload']")
+
+# Construct the query parameters
+params = json.load(open("Parameters.json"))["Parameters"]
+filepath = json.load(open("Parameters.json"))["filepath"]
+
+form_action_url = form_elem.get_attribute('action')
+full_url = f"{form_action_url}?{urlencode(params)}"
+
+user_agent = driver.execute_script("return navigator.userAgent;")
+headers = {
+    'User-Agent': user_agent,
+    'Referer': full_url,  # Add any other headers as needed
+}
+
+response = requests.get(full_url, cookies=cookies, headers=headers)
+
+response
+
+#Overwrites current data. Becareful
+with open(filepath, "wb") as file:
+    file.write(response.content)
+
+driver.quit()
